a834f85fdb730bd18dfc409c1e7a130ec014ea3c
luisdavidMG <101727868+luisdavidMG@users.noreply.github.com>
2022-04-08 12:54:51 -0500

    Create UTILS.H

---
+++ UNIT 1/UTILS.H
@@ -0,0 +1,11 @@
+int isGreaterThan10(int number){
+    int resultado = 0;
+    if (number > 10){
+        resultado = 1;
+    } else {
+        resultado = 0;
+    }
+    
+     printf("The number is: %d\n", number);
+    return (resultado);
+}
