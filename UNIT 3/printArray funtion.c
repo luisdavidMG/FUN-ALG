f1b22b313a0ca086f0e62db368dd793bb5ea9c96
luisdavidMG <101727868+luisdavidMG@users.noreply.github.com>
2022-04-08 13:01:53 -0500

    Add files via upload

---
+++ UNIT 3/printArray funtion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void printf_array8int array{3},int len_array){
+    int i = 0;
+    printf("{");
+    while (i < len_array){
+        printf("%d", array(i)){
+            if(i< len_array-1){
+                printf(",");
+}
+i=i+1;
+printf("}");
+}
+
+
+int main(){
+    int len_array = 3
+    int array (3) = {5,2, 3};
+    printf_array (array,3);
+    return 0;
+}
