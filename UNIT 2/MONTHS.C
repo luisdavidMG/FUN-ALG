67febd53f09facad87b204ed9c8c7c83daa942f5
luisdavidMG <101727868+luisdavidMG@users.noreply.github.com>
2022-04-08 12:57:33 -0500

    Create MONTHS.C

---
+++ UNIT 2/MONTHS.C
@@ -0,0 +1,110 @@
+#include <stdio.h>
+int main(){
+    int part1 = 31;
+    int part2 = 28;
+    int part3= 30;
+    int month;
+	printf("Give me the number of any month ( 1-12):\n");
+	scanf("%d", &month);
+    
+	if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 1) {
+        printf("January has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 2) {
+        printf("February has 28 days\n");
+        for (int i = 1; i <=part2; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 3) {
+        printf("March has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 4) {
+        printf("April has 30 days\n");
+        for (int i = 1; i <=part3; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 5) {
+        printf("May has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 6) {
+        printf("June has 30 days\n");
+        for (int i = 1; i <=part3; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 7) {
+        printf("July has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 8) {
+        printf("August has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 9) {
+        printf("September has 30 days\n");
+        for (int i = 1; i <=part3; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 10) {
+        printf("October has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 11) {
+        printf("November has 30 days\n");
+        for (int i = 1; i <=part3; i++){
+            printf("%d\n", i);
+        }
+    }
+    if(month >=13){
+		printf("Error; please give other number of the 1-12\n" );
+	}else if(month == 12) {
+        printf("December has 31 days\n");
+        for (int i = 1; i <=part1; i++){
+            printf("%d\n", i);
+        }
+    }
+	return 0;
+}
+© 2022 GitHub, Inc.
+Terms
+Priva
